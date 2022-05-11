adac26b230cf18c487dea8d8659e755a06c10caf
Alex Grafen <alex.grafen@gmail.com>
2022-05-11 15:12:54 +0100

    add page where posts display

--- server.js
+++ server.js
@@ -32,3 +32,36 @@ server.get("/", (request, response) => {
 `;
     response.send(html);
 });
+
+server.get("/show-posts", (request, response) => {
+    item = "";
+    console.log(users);
+    for (const user of Object.values(users)) {
+        item += `<li><div><h3>${user.username}</h3> <p>${user.post}</p></div></li>`;
+    }
+    const html = /* html */`
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>bløgge</title>
+    </head>
+    
+    <body>
+        <h1>bløgge</h1>
+        <form>
+            <label for="username">
+                <input name="username" id="username" />
+            </label>
+            <label for="post">
+                <input name="post" id="post" />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+        <section>
+        <h2>Recent Posts</h2>
+        <ul>${item}</ul>
+        </section>
+  </body>
+</html>`
+    response.send(html)
+})
