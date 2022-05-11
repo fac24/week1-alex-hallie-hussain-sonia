cc773cdb049af028144d2d098c2e03a0f7ac5983
Alex Grafen <alex.grafen@gmail.com>
2022-05-11 15:52:28 +0100

    render posts on home page

--- server.js
+++ server.js
@@ -7,35 +7,6 @@ const PORT = 3000;
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 server.get("/", (request, response) => {
-
-    const html = /* html */`
-    <html>
-    <head>
-        <meta charset="utf-8">
-        <title>bløgge</title>
-    </head>
-    
-    <body>
-        <h1>bløgge</h1>
-        <form>
-            <label for="username">
-                <input name="username" id="username" />
-            </label>
-            <label for="post">
-                <input name="post" id="post" />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-
-  </body>
-</html>
-`;
-    response.send(html);
-});
-
-// app.use(express.static("/" + '/public'));
-
-server.get("/show-posts", (request, response) => {
     item = "";
     for (const user of Object.values(users)) {
         item += `<li><div><h3>${user.username}</h3> <p>${user.post}</p></div></li>`;
@@ -49,7 +20,7 @@ server.get("/show-posts", (request, response) => {
     </head>
     
     <body>
-        <h1 style="color:blue;text-align:center;">bløgge</h1>
+        <h1>bløgge</h1>
         <form>
             <label for="username">
                 <input name="username" id="username" />
@@ -66,4 +37,6 @@ server.get("/show-posts", (request, response) => {
   </body>
 </html>`
     response.send(html)
-})
+});
+
+// app.use(express.static("/" + '/public'));
