67beac3937ff77a3825c222d9184a3fe93a31034
Alex Grafen <alex.grafen@gmail.com>
2022-05-13 09:03:05 +0100

    update tests to bring in line

--- cypress/integration/basic-tests.js
+++ cypress/integration/basic-tests.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-it("can visit homepage", () => {
-=======
 it("user can visit homepage", () => {
->>>>>>> 8c0a4339786a560ca9987668744f63b6f4877ad4
     cy.visit("https://micro-blogge.herokuapp.com/");
 })
 
@@ -20,25 +16,6 @@ it("user can read posts on home page", () => {
 it("user can read more than one post", () => {
     cy.visit("https://micro-blogge.herokuapp.com/");
     cy.get("ul")
-<<<<<<< HEAD
-    .children()
-    .should('contain', 'The best post')
-    .and('contain', 'The worst post')
-})
-
-it("user can submit post and post will appear beneath", () => {
-    cy.visit("http://localhost:3000");
-    cy.get('form[data-cy="posting-form"]').find('input[data-cy="username"]').type("Theodore Roosevelt");
-    cy.get('form[data-cy="posting-form"]').find('input[data-cy="post"]').type('Speak f');
-    cy.get('form[data-cy="posting-form"]').find('button[data-cy="submit"]').click();
-    cy.get('ul>li').contains("h3", "alex");
-    cy.get('ul>li').contains("p", "test");
-})
-
-it("user can delete a post", () => {
-    cy.visit("https://micro-blogge.herokuapp.com/");
-    
-=======
         .children()
         .should('contain', 'The best post')
         .and('contain', 'The worst post')
@@ -63,7 +40,6 @@ it("user can delete a post", () => {
     cy.contains('Theodore Roosevelt').should('exist');
     cy.get('button[name="name"]').last().click();
     cy.contains('Theodore Roosevelt').should('not.exist');
->>>>>>> 8c0a4339786a560ca9987668744f63b6f4877ad4
 })
 
 //routes
